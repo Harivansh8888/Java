ddbd11da43f4ffc2f6ab4c58d9ac8213a99121d4
Harivansh Mathur <mathurharivansh811@gmail.com>
2020-01-19 15:03:18 +0530

    Fibonacci Series, only printing first 2 numbers now

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci
+{
+	public static void main(String args[])
+	{
+		int n=Integer.parseInt(args[0]);
+		int i,a,b,c;
+		a=0;
+		b=1;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
