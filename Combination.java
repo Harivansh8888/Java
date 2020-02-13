38b5f65d3813a56955dbcbe7bea65a40b8a3eb34
Harivansh Mathur <mathurharivansh811@gmail.com>
2020-02-13 21:48:56 +0530

    Combination from 3 numbers - input module

---
+++ Combination.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Combination
+{
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+  	int[] input = new int[3];
+	for(int i=0;i<3;i++)
+	{
+		input[i] =sc.nextInt();
+	}
+	}
+}
