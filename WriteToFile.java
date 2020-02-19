8c8c07a7dc7e136c5983070053e09d2a4aff372d
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-19 19:18:02 +0530

    added comments

--- WriteToFile.java
+++ WriteToFile.java
@@ -7,15 +7,15 @@ public class WriteToFile
 	{
 		try
 		{
-			FileWriter myWriter = new FileWriter("filename.txt");
-			myWriter.write("Learning file handling in java");
-			myWriter.close();
+			FileWriter myWriter = new FileWriter("filename.txt"); // FileWriter is a standard class in Java
+			myWriter.write("Learning file handling in java"); // .write() for writing data into file
+			myWriter.close(); // .close() closes the file
 			System.out.println("Successfully wrote to the file");
 		}
 		catch(IOException e)
 		{
 			System.out.println("An error occured");
-			e.printStackTrace();
+			e.printStackTrace(); // prints information about where error have occured
 		}
 	}
 }
