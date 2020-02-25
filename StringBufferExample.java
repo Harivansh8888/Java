ba2826650cf0e4bad2939970f163606e14852ae5
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-25 21:18:07 +0530

    added all the switch cases

--- StringBufferExample.java
+++ StringBufferExample.java
@@ -13,5 +13,31 @@ class StringBufferExample
 	System.out.println("3.Replace");
 	System.out.println("4.Delete");
 	System.out.println("5.Reverse");
+	int ch = sc.nextInt();
+	switch (ch)
+	{
+	case 1:
+	sb.append("Java");
+	System.out.println(sb);
+	break;
+	case 2:
+	sb.insert(2,"Java");
+	System.out.println(sb);
+	break;
+	case 3:
+	sb.replace(1,3,"Java");
+	System.out.println(sb);
+	break;
+	case 4:
+	sb.delete(1,3);
+	System.out.println(sb);
+	break;
+	case 5:
+	sb.reverse();
+	System.out.println(sb);
+	break;
+	default:
+	System.out.println("Wrong Choice Entered !");
+	break;
   }
 }
