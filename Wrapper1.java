3cdf6ce48b37a28b9c81a11c2eb9eba1791f31e7
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-26 19:48:11 +0530

    Wrapper class implementation - 1

---
+++ Wrapper1.java
@@ -0,0 +1,12 @@
+public class Wrapper1
+{
+	public static void main(String args[])
+	{
+	Integer myInt = 5;
+	Double myDouble = 5.22;
+	Character myChar = 'A';
+	System.out.println(myInt);
+	System.out.println(myDouble);
+	System.out.println(myChar);
+	}
+}
