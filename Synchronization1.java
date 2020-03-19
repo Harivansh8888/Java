5a28b2a4a11f97c48f76684465dc26179400a42c
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-03-19 13:50:47 +0530

    added Synchronization1 class with main function

--- Synchronization1.java
+++ Synchronization1.java
@@ -31,3 +31,13 @@ class MyThread2 extends Thread {
 		t.printTable(100);
 	}
 }
+public class Synchronization1 {
+	public static void main(String args[])
+	{
+		Table obj = new Table();
+		MyThread1 t1 = new MyThread1(obj);
+		MyThread2 t2 = new MyThread2(obj);
+		t1.start();
+		t2.start();
+	}
+}
