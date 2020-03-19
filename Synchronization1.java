a2dfb0a9f195e79c606ad02a15eff105390c32cb
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-03-19 13:50:00 +0530

    added MyThread2 subclass

--- Synchronization1.java
+++ Synchronization1.java
@@ -22,3 +22,12 @@ class MyThread1 extends Thread {
 		t.printTable(5);
 	}
 }
+class MyThread2 extends Thread {
+	Table t;
+	MyThread2(Table t) {
+		this.t =t;
+	}
+	public void run() {
+		t.printTable(100);
+	}
+}
