c11e3ed825147f9a93c7b06d337e34148ca61b4c
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-03-24 13:34:39 +0530

    added Threading2 class

--- OnlineThreadingAssignment.java
+++ OnlineThreadingAssignment.java
@@ -51,3 +51,50 @@ class Threading1 extends Thread
     }
    }
 }
+class Threading2 extends Thread
+{
+	double count = 0,countBuffer=0,countLine=0;
+    String lineNumber = "";
+    String filePath = "C:\\Users\\hp\\Desktop\\Document2.txt";
+    String line = "";
+    BufferedReader br;
+    String inputSearch = "harivansh";
+    public void run() {
+	try {
+        br = new BufferedReader(new FileReader(filePath));
+        try {
+            while((line = br.readLine()) != null)
+            {
+                countLine++;
+                System.out.println("Document 2 is :");
+                System.out.println(line);
+                String[] words = line.split(" ");
+
+                for (String word : words) {
+                  if (word.equals(inputSearch)) {
+                    count++;
+                    countBuffer++;
+                  }
+                }
+
+                if(countBuffer > 0)
+                {
+                    countBuffer = 0;
+                    lineNumber += countLine + ",";
+                }
+
+            }
+            System.out.println("Times found at--"+count);
+            System.out.println("Word found at--"+lineNumber);
+            br.close();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    } 
+	catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+   }
+}
+
