99f11905ec74b22e90750d16c9fb13dbf97bf109
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-03-24 13:34:01 +0530

    added Threading1 class

--- OnlineThreadingAssignment.java
+++ OnlineThreadingAssignment.java
@@ -4,3 +4,50 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+class Threading1 extends Thread
+{
+	double count = 0,countBuffer=0,countLine=0;
+    String lineNumber = "";
+    String filePath = "C:\\Users\\hp\\Desktop\\Document1.txt";
+    BufferedReader br;
+    String inputSearch = "java";
+    String line = "" ;
+    public void run() {
+	try {
+        br = new BufferedReader(new FileReader(filePath));
+        try {
+            	while((line = br.readLine()) != null)
+            	{
+            		countLine++;
+            		System.out.println("Document 1 is :");
+            		System.out.println(line);
+            		String[] words = line.split(" ");
+
+            		for (String word : words) {
+            			if (word.equals(inputSearch)) {
+            				count++;
+            				countBuffer++;
+            			}
+            		}
+
+            		if(countBuffer > 0)
+            		{
+            			countBuffer = 0;
+            			lineNumber += countLine + ",";
+            		}
+
+            	}
+            	System.out.println("Times found at--"+count);
+            	System.out.println("Word found at--"+lineNumber);
+            	br.close();
+        	} 
+       catch (IOException e) {
+    	   e.printStackTrace();
+       }
+    } 
+	catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+   }
+}
